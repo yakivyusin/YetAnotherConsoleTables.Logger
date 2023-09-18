c1385a5973ce456858d911565e437f35f6a0ee9d
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-09-18 15:27:30 +0300

    Add Base Logger Extensions

---
+++ YetAnotherConsoleTables.Logger/LoggerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YetAnotherConsoleTables.Logger;
+
+public static class LoggerExtensions
+{
+    public static void LogTable<T>(this ILogger logger, LogLevel logLevel, IEnumerable<T> collection) => logger.LogTable(logLevel, 0, collection, ConsoleTableFormat.Default);
+
+    public static void LogTable<T>(this ILogger logger, LogLevel logLevel, EventId eventId, IEnumerable<T> collection) => logger.LogTable(logLevel, eventId, collection, ConsoleTableFormat.Default);
+
+    public static void LogTable<T>(this ILogger logger, LogLevel logLevel, IEnumerable<T> collection, ConsoleTableFormat tableFormat) => logger.LogTable(logLevel, 0, collection, tableFormat);
+
+    public static void LogTable<T>(this ILogger logger, LogLevel logLevel, EventId eventId, IEnumerable<T> collection, ConsoleTableFormat tableFormat)
+    {
+        using var writer = new StringWriter();
+
+        writer.WriteLine();
+        ConsoleTable.From(collection).Write(tableFormat, writer);
+        
+        logger.Log(logLevel, eventId, writer.ToString());
+    }
+}
